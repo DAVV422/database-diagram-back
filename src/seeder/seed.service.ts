8ac309872117c9a3af1413d836756dc2dc68c242
diegov67 <vargas.diego@ficct.uagrm.edu.bo>
2024-04-13 05:23:27 -0400

    feature: project base

---
+++ src/seeder/seed.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, Logger } from '@nestjs/common';
+
+import { handlerError } from '../common/utils/handlerError.utils';
+import { ROLES } from '../common/constants';
+import { CreateUserDto } from '../user/dto';
+import { UserService } from '../user/services/user.service';
+
+@Injectable()
+export class SeedService {
+  private readonly logger = new Logger('SeederService');
+
+  constructor(private readonly userService: UserService) { }
+
+  public async runSeeders() {
+    if (process.env.APP_PROD == true) return { message: 'No se puede ejecutar seeders en producción' };
+    try {
+      const user: CreateUserDto = {
+        nombre: 'diego',
+        apellido: 'vargas',
+        email: 'diego@live.com',
+        password: '123456789',
+        role: ROLES.ADMIN,
+      }
+      await this.userService.createUser(user);
+      return { message: 'Seeders ejecutados correctamente' };
+    } catch (error) {
+      handlerError(error, this.logger);
+    }
+  }
+}
---
+++ src/user/controllers/user.controller.ts
@@ -0,0 +1,57 @@
+import { Body, Controller, Get, Delete, Param, UseGuards, ParseUUIDPipe, Query, Patch, } from '@nestjs/common';
+import { ApiBearerAuth, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger/dist';
+
+import { RolesAccess } from '../../auth/decorators/roles.decorator';
+import { AuthGuard, RolesGuard } from '../../auth/guards/';
+import { UpdateUserDto } from '../dto/';
+import { UserService } from '../services/user.service';
+import { QueryDto } from '../../common/dto/query.dto';
+import { ResponseMessage } from 'src/common/interfaces/responseMessage.interface';
+
+@ApiTags('User')
+@ApiBearerAuth()
+@UseGuards(AuthGuard, RolesGuard)
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @ApiQuery({ name: 'limit', type: 'number', required: false })
+  @ApiQuery({ name: 'offset', type: 'number', required: false })
+  @ApiQuery({ name: 'order', type: 'string', required: false })
+  @ApiQuery({ name: 'attr', type: 'string', required: false })
+  @ApiQuery({ name: 'value', type: 'string', required: false })
+  @Get()
+  public async findAll(@Query() queryDto: QueryDto): Promise<ResponseMessage> {
+    return {
+      statusCode: 200,
+      data: await this.userService.findAll(queryDto),
+    };
+  }
+
+  @ApiParam({ name: 'id', type: 'string' })
+  @Get(':id')
+  public async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseMessage> {
+    return {
+      statusCode: 200,
+      data: await this.userService.findOne(id),
+    }
+  }
+
+  @ApiParam({ name: 'id', type: 'string' })
+  @Patch(':id')
+  public async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto): Promise<ResponseMessage> {
+    return {
+      statusCode: 200,
+      data: await this.userService.update(id, updateUserDto),
+    };
+  }
+
+  @RolesAccess('ADMIN')
+  @ApiParam({ name: 'id', type: 'string' })
+  @Delete(':id')
+  public async delete(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseMessage> {
+    return await this.userService.delete(id);
+  }
+}
