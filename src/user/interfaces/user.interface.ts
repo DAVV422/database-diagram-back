70a3839b50b62efae0e87751e642d89de2a0dc65
diegov67 <vargas.diego@ficct.uagrm.edu.bo>
2024-04-25 12:05:34 -0400

    fix

--- src/user/interfaces/user.interface.ts
+++ src/user/interfaces/user.interface.ts
@@ -1,16 +1,7 @@
 export interface IUser {
-<<<<<<< HEAD
     nombre: string;
     apellido: string;
     email: string;    
-=======
-    name: string;
-    last_name: string;
-    email: string;
-    cellphone: string;
-    grade: string;
-    birthdate: Date;
->>>>>>> fec11c61a0e50370bf51b730daa7f5ac00ea3025
     password: string;
     role: string;
   }
